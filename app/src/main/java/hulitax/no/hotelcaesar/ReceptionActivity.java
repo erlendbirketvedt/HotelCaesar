e44c44475ca0c219ee4314c9d41777aea9ecffe6
erlendbirketvedt <erlend.birketvedt@gmail.com>
2017-05-28 11:01:04 +0200

    Initial commit

--- app/src/main/java/hulitax/no/hotelcaesar/ReceptionActivity.java
+++ app/src/main/java/hulitax/no/hotelcaesar/ReceptionActivity.java
@@ -3,7 +3,7 @@ package hulitax.no.hotelcaesar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ReceptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
